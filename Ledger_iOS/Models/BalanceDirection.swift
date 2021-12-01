0252609004e69013d6b0429a7b9bca626077a6b6
Elisha Chirchir <elisha.java@gmail.com>
2021-12-02 01:19:53 +0300

    adds realm integration (#9)

---
+++ Ledger_iOS/Models/BalanceDirection.swift
@@ -0,0 +1,14 @@
+//
+//  BalanceDirection.swift
+//  Ledger_iOS
+//
+//  Created by Elisha Chirchir on 13/11/2021.
+//
+
+import Foundation
+
+enum BalanceDirection: Int, Codable, Equatable  {
+    case positive = 1 // Net gain
+    case negative = -1 //Net loss
+    case neutral = 0//No gain or loss
+}
