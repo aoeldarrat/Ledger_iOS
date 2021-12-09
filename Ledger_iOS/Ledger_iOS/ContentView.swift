de67b053d91fde470d4cf9eccfbe164dbcff99d9
Abdulrahman Eldarrat <abdul.eldarrat@quarkworks.co>
2021-12-09 16:55:53 -0600

    Add new project with gitignore

---
+++ Ledger_iOS/Ledger_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ledger_iOS
+//
+//  Created by Abdulrahman Eldarrat on 12/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ledger_iOS/Ledger_iOS/Ledger_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ledger_iOSApp.swift
+//  Ledger_iOS
+//
+//  Created by Abdulrahman Eldarrat on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Ledger_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
